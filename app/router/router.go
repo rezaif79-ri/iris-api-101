4cd2048c991d06abf10ac3a073cec112e7aabd50
Reza IF <rezaif79.ri@gmail.com>
2023-12-15 23:46:58 +0700

    feat: fix implementation iris app routing

--- app/router/router.go
+++ app/router/router.go
@@ -1,9 +1,18 @@
 package router
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/rezaif79-ri/iris-api-101/app/controller"
+)
 
 func SetupIrisRouter(app *iris.Application) *iris.Application {
+	// Init controller
+	bookController := controller.NewBookController()
+
 	books := app.Party("books")
 	books.Use(iris.Compression)
+	books.Get("", bookController.GetList)
+	books.Post("", bookController.CreateBook)
+
 	return app
 }
