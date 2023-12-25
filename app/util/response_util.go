2dbcf0f0ec121dfb10e699d7801937c5d92fe532
Reza IF <rezaif79.ri@gmail.com>
2023-12-25 23:46:13 +0700

    feat: add map string type in package util

--- app/util/response_util.go
+++ app/util/response_util.go
@@ -1,5 +1,7 @@
 package util
 
+type MapString map[string]interface{}
+
 func RestWrapperObject(status int, message string, data interface{}) map[string]interface{} {
 	return map[string]interface{}{
 		"status":  status,
