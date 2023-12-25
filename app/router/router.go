f2d573811c32b16ff0e88257c96fc34bebdff874
Reza IF <rezaif79.ri@gmail.com>
2023-12-25 23:51:23 +0700

    fix: temp fix removing defer disconect mongo conneciton

--- app/router/router.go
+++ app/router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"context"
-
 	"github.com/kataras/iris/v12"
 	"github.com/rezaif79-ri/iris-api-101/app/config"
 	"github.com/rezaif79-ri/iris-api-101/app/controller"
@@ -14,11 +12,11 @@ func SetupIrisRouter(app *iris.Application) *iris.Application {
 	if err != nil {
 		panic("setup mongo conn error: " + err.Error())
 	}
-	defer func() {
-		if err = mongoDb.Client().Disconnect(context.Background()); err != nil {
-			panic(err)
-		}
-	}()
+	// defer func() {
+	// 	if err = mongoDb.Client().Disconnect(context.Background()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
 
 	// Init controller
 	bookController := controller.NewBookController(mongoDb)
