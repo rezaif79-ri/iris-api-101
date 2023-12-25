1d0384da6f7fe55796f81752a25e829154bb3b1d
Reza IF <rezaif79.ri@gmail.com>
2023-12-25 19:22:54 +0700

    feat: add config package and add setup mongo conn method

---
+++ app/config/mongo_config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func SetupMongoConn() (*mongo.Database, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client()
+	clientOptions.ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Database("books_database"), nil
+}
