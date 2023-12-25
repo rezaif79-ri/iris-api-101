24c37360104e8e921d0291d840cb0b50556edc19
Reza IF <rezaif79.ri@gmail.com>
2023-12-25 23:56:46 +0700

    fix: mongo client disconected on setup router

--- app/main.go
+++ app/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
+
 	"github.com/kataras/iris/v12"
+	"github.com/rezaif79-ri/iris-api-101/app/config"
 	"github.com/rezaif79-ri/iris-api-101/app/router"
 )
 
 func main() {
+	// Init mongo db
+	mongoDb, err := config.OpenMongoBookDB()
+	if err != nil {
+		panic("setup mongo conn error: " + err.Error())
+	}
+
 	app := iris.New()
 
-	router.SetupIrisRouter(app)
+	router.SetupIrisRouter(app, mongoDb)
 
 	app.Listen("127.0.0.1:8080")
+	defer func() {
+		if err = mongoDb.Client().Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}()
 }
--- app/router/router.go
+++ app/router/router.go
@@ -2,22 +2,11 @@ package router
 
 import (
 	"github.com/kataras/iris/v12"
-	"github.com/rezaif79-ri/iris-api-101/app/config"
 	"github.com/rezaif79-ri/iris-api-101/app/controller"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func SetupIrisRouter(app *iris.Application) *iris.Application {
-	// Init mongo db
-	mongoDb, err := config.SetupMongoConn()
-	if err != nil {
-		panic("setup mongo conn error: " + err.Error())
-	}
-	// defer func() {
-	// 	if err = mongoDb.Client().Disconnect(context.Background()); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
-
+func SetupIrisRouter(app *iris.Application, mongoDb *mongo.Database) *iris.Application {
 	// Init controller
 	bookController := controller.NewBookController(mongoDb)
 
