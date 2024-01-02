df706a94dbefc017c40c6f5c9084cd1726e7ed0a
Reza IF <rezaif79.ri@gmail.com>
2024-01-02 22:53:40 +0700

    feat: add get env on open mongo book db connection

--- app/config/mongo_config.go
+++ app/config/mongo_config.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/rezaif79-ri/iris-api-101/app/util"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -12,8 +13,15 @@ func OpenMongoBookDB() (*mongo.Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	// Get mongo connection from ENV
+	mongoHost := util.GetEnv("MONGO_HOST", "localhost")
+	mongoPort := util.GetEnv("MONGO_PORT", "27027")
+	mongoProtocol := util.GetEnv("MONGO_PROTOCOL", "mongodb")
+	mongoCreds := util.GetEnv("MONGO_CREDS", "")
+
+	connUri := mongoProtocol + "://" + mongoCreds + mongoHost + ":" + mongoPort
 	clientOptions := options.Client()
-	clientOptions.ApplyURI("mongodb://localhost:27027")
+	clientOptions.ApplyURI(connUri)
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		return nil, err
