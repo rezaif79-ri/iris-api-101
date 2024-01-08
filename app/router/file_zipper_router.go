a9459b680f15f2e88c71b5c40e602ab2aa620729
Reza IF <rezaif79.ri@gmail.com>
2024-01-08 22:17:30 +0700

    feat: add file zipper router

---
+++ app/router/file_zipper_router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/core/router"
+	"github.com/rezaif79-ri/iris-api-101/app/controller"
+)
+
+func AddFileZipperRouter(group router.Party) {
+	// Init file zipper controller
+	zipper := controller.NewFileZipController()
+
+	group.Use(iris.Compression)
+	group.Post("zipper/single", zipper.ZipOneFile)
+}
