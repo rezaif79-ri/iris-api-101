6536e3fbc2890000d084fab2cd18b3db9f767369
Reza IF <rezaif79.ri@gmail.com>
2024-01-09 22:35:41 +0700

    feat: add custom iris logger middleware

--- app/main.go
+++ app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/accesslog"
 	"github.com/rezaif79-ri/iris-api-101/app/config"
 	"github.com/rezaif79-ri/iris-api-101/app/router"
 	"github.com/rezaif79-ri/iris-api-101/app/util"
@@ -20,6 +22,7 @@ func main() {
 	if util.GetEnv("API_DEBUG", "dev") == "dev" {
 		app.Logger().SetLevel("debug")
 	}
+	app.UseRouter(customIrisLog().Handler)
 
 	router.SetupIrisRouter(app, mongoDb)
 
@@ -31,4 +34,35 @@ func main() {
 			panic(err)
 		}
 	}()
+}
+
+func customIrisLog() *accesslog.AccessLog {
+	// Initialize a new access log middleware.
+	ac := accesslog.New(os.Stdout)
+	// The default configuration:
+	ac.Delim = '|'
+	ac.TimeFormat = "2006-01-02 15:04:05"
+	ac.Async = false
+	ac.IP = true
+	ac.BytesReceivedBody = true
+	ac.BytesSentBody = true
+	ac.BytesReceived = false
+	ac.BytesSent = false
+	ac.BodyMinify = true
+	ac.RequestBody = true
+	ac.ResponseBody = false
+	ac.KeepMultiLineError = true
+	ac.PanicLog = accesslog.LogHandler
+
+	// Default line format if formatter is missing:
+	// Time|Latency|Code|Method|Path|IP|Path Params Query Fields|Bytes Received|Bytes Sent|Request|Response|
+	//
+	// Set Custom Formatter:
+	ac.SetFormatter(&accesslog.JSON{
+		Indent:    "  ",
+		HumanTime: true,
+	})
+	// ac.SetFormatter(&accesslog.CSV{})
+	// ac.SetFormatter(&accesslog.Template{Text: "{{.Code}}"})
+	return ac
 }
