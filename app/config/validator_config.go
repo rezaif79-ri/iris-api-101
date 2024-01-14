912764a8459ab43fab04f8b3d69661968e2b3ce0
Reza IF <rezaif79.ri@gmail.com>
2024-01-14 18:44:45 +0700

    docs: add documentation for new validator register

--- app/config/validator_config.go
+++ app/config/validator_config.go
@@ -2,6 +2,11 @@ package config
 
 import "github.com/go-playground/validator/v10"
 
+/*
+	NewValidatorRegister func create a new validator instance
+	Adds a custom validations with given tags in this config package
+	And returning pointer validator.Validate
+*/
 func NewValidatorRegister() *validator.Validate {
 	v := validator.New()
 	v.RegisterValidation("alphabet_only", AlphabetOnly)
