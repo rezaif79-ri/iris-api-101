9632ec98e5acd728133b04ff9a5fa8ab7df4709c
Reza IF <rezaif79.ri@gmail.com>
2024-01-14 17:09:40 +0700

    feat: add aes util to encrypt and decrypt data

---
+++ app/util/aes_util/aes.go
@@ -0,0 +1,52 @@
+package aesUtil
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+)
+
+func AESEncrypt(src string, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if src == "" {
+		return nil, errors.New("plain content empty")
+	}
+	ecb := cipher.NewCBCEncrypter(block, iv)
+	content := []byte(src)
+	content = PKCS5Padding(content, block.BlockSize())
+	crypted := make([]byte, len(content))
+	ecb.CryptBlocks(crypted, content)
+
+	return crypted, nil
+}
+
+func AESDecrypt(crypt []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(crypt) == 0 {
+		return nil, errors.New("plain content empty")
+	}
+	ecb := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(crypt))
+	ecb.CryptBlocks(decrypted, crypt)
+
+	return PKCS5Trimming(decrypted), nil
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
