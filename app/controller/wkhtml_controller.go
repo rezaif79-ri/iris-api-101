71093604e62a3fa951e8d9f1d80392646bee3fe2
Reza IF <rezaif79.ri@gmail.com>
2024-01-20 18:53:32 +0700

    feat: add multi html convert pdf controller implementation

--- app/controller/wkhtml_controller.go
+++ app/controller/wkhtml_controller.go
@@ -63,3 +63,49 @@ func (wci *WkhtmlControllerImpl) ConvertHtmlToPDF(ctx *context.Context) {
 	pdfData := bytes.NewReader(pdfg.Bytes())
 	ctx.ServeContent(pdfData, "test", time.Now())
 }
+
+// ConvertMultiHtmlToPDF implements domain.WkhtmlController.
+func (wci *WkhtmlControllerImpl) ConvertMultiHtmlToPDF(ctx *context.Context) {
+	var b domain.MultiHtmlConvertPDF
+	err := ctx.ReadJSON(&b)
+	if err != nil {
+		ctx.StopWithJSON(http.StatusConflict, util.RestWrapperObject(http.StatusConflict, "FAIL", util.MapString{
+			"error": err.Error(),
+		}))
+		return
+	}
+
+	pdfg, err := wci.NewPDFGenerator()
+	if err != nil {
+		ctx.StopWithJSON(http.StatusInternalServerError, util.RestWrapperObject(
+			http.StatusInternalServerError, "FAIL",
+			util.MapString{
+				"status":  http.StatusInternalServerError,
+				"message": err.Error(),
+				"data":    err,
+			}))
+		return
+	}
+
+	for i := range b.Data {
+		// Add to document
+		pdfg.AddPage(wci.NewPDFPageReader(bytes.NewBuffer(b.Data[i])))
+
+	}
+
+	// Create PDF document in internal buffer
+	err = pdfg.Create()
+	if err != nil {
+		ctx.StopWithJSON(http.StatusInternalServerError, util.RestWrapperObject(
+			http.StatusInternalServerError, "FAIL",
+			util.MapString{
+				"status":  http.StatusInternalServerError,
+				"message": err.Error(),
+				"data":    err,
+			}))
+		return
+	}
+
+	pdfData := bytes.NewReader(pdfg.Bytes())
+	ctx.ServeContent(pdfData, "test", time.Now())
+}
