5e10cbacaf8a43f0f805a81638fa4cfafaed2ff8
Reza IF <rezaif79.ri@gmail.com>
2024-01-20 18:50:32 +0700

    feat: add multi html convert pdf controller interface

--- app/domain/wkhtml.go
+++ app/domain/wkhtml.go
@@ -6,6 +6,11 @@ type HtmlConvertPDF struct {
 	Data []byte `json:"data"`
 }
 
+type MultiHtmlConvertPDF struct {
+	Data [][]byte `json:"data"`
+}
+
 type WkhtmlController interface {
 	ConvertHtmlToPDF(iris.Context)
+	ConvertMultiHtmlToPDF(iris.Context)
 }
