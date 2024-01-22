8701f1542e0526a654860162d6e7ce75a6a57e6e
Reza IF <rezaif79.ri@gmail.com>
2024-01-22 20:11:13 +0700

    feat: add http query param on http util package

---
+++ app/util/http_util/query.go
@@ -0,0 +1,29 @@
+package httputil
+
+import "strings"
+
+type httpQueryParams struct {
+	query map[string]string
+}
+
+func NewHTTPQuery() *httpQueryParams {
+	return &httpQueryParams{
+		query: make(map[string]string),
+	}
+}
+
+func (q *httpQueryParams) SetQuery(key, value string) {
+	q.query[key] = value
+}
+
+func (q *httpQueryParams) QueryStringURL() string {
+	if len(q.query) == 0 {
+		return ""
+	}
+	var query string = "?"
+	for k, v := range q.query {
+		query += k + "=" + v + "&"
+	}
+
+	return strings.TrimSuffix(query, "&")
+}
