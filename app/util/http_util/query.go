5df608ce30820bd2f0430ce4f5752b9e9308541e
Reza IF <rezaif79.ri@gmail.com>
2024-01-22 20:13:19 +0700

    feat: add reset http query param util package

--- app/util/http_util/query.go
+++ app/util/http_util/query.go
@@ -16,6 +16,14 @@ func (q *httpQueryParams) SetQuery(key, value string) {
 	q.query[key] = value
 }
 
+func (q *httpQueryParams) Reset() bool {
+	if len(q.query) == 0 {
+		return false
+	}
+	q.query = make(map[string]string)
+	return true
+}
+
 func (q *httpQueryParams) QueryStringURL() string {
 	if len(q.query) == 0 {
 		return ""
