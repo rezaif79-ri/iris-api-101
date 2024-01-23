267fc739489189bec73740dc680f4e40a9274992
Reza IF <rezaif79.ri@gmail.com>
2024-01-23 16:48:33 +0700

    feat: add get api and post api func on http util package

--- app/util/http_util/client.go
+++ app/util/http_util/client.go
@@ -1,14 +1,44 @@
 package httputil
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"time"
 )
 
-type HTTPClient http.Client
+type HTTPClient struct {
+	client *http.Client
+}
 
-func NewHTTPClient(timeout time.Duration) *HTTPClient {
-	return &HTTPClient{
-		Timeout: timeout,
+type HTTPResponse struct {
+}
+
+func NewHTTPClient(timeout time.Duration) HTTPClient {
+	return HTTPClient{
+		client: &http.Client{
+			Timeout: timeout,
+		},
 	}
+}
+
+func (h *HTTPClient) GetAPI(uri string, callback func(*http.Response, error)) {
+	res, err := h.client.Get(uri)
+	if err != nil {
+		callback(nil, err)
+		return
+	}
+	defer res.Body.Close()
+	callback(res, err)
+}
+
+func (h *HTTPClient) PostAPI(uri string, body interface{}, callback func(*http.Response, error)) {
+	bodyLoad, err := json.Marshal(body)
+	res, err := h.client.Post(uri, "application/json", bytes.NewReader(bodyLoad))
+	if err != nil {
+		callback(nil, err)
+		return
+	}
+	defer res.Body.Close()
+	callback(res, err)
 }
