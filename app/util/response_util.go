421fb19f17051892392ffe4f263378268c4ac366
Reza IF <rezaif79.ri@gmail.com>
2024-01-29 19:55:07 +0700

    feat: add iris json response function on util package

--- app/util/response_util.go
+++ app/util/response_util.go
@@ -1,5 +1,7 @@
 package util
 
+import "github.com/kataras/iris/v12/context"
+
 type MapString map[string]interface{}
 
 func RestWrapperObject(status int, message string, data interface{}) map[string]interface{} {
@@ -8,4 +10,15 @@ func RestWrapperObject(status int, message string, data interface{}) map[string]
 		"message": message,
 		"data":    data,
 	}
+}
+
+// IrisJSONResponse will write response to client by iris context
+func IrisJSONResponse(ctx *context.Context, statusCode int, message string, data interface{}) {
+	ctx.StatusCode(statusCode)
+	ctx.JSON(RestWrapperObject(statusCode, message, MapString{
+		"status_code": statusCode,
+		"message":     message,
+		"data":        data,
+	}))
+	return
 }
