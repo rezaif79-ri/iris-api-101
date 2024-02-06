2509b7695e49936ae5b43c8a79e48b2878af66a9
Reza IF <rezaif79.ri@gmail.com>
2024-02-06 16:00:55 +0700

    feat: add map response status message http util

--- app/util/http_util/response.go
+++ app/util/http_util/response.go
@@ -14,3 +14,19 @@ type HTTPResponse struct {
 func convertBodyToBytes(body io.ReadCloser) ([]byte, error) {
 	return io.ReadAll(body)
 }
+
+var mapResponseStatusMessage map[int]string = map[int]string{
+	200: "http util: success",
+	201: "http util: success",
+	202: "http util: success",
+
+	400: "http util: bad request",
+	401: "http util: unauthorized",
+	402: "http util: payment required",
+	403: "http util: forbidden",
+	404: "http util: not found",
+
+	500: "http util: internal server error",
+	501: "http util: unimplemented",
+	502: "http util: bad gateway",
+}
